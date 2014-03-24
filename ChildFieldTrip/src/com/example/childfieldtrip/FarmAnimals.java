f24b2081a7485cda0bbf18cfeb8f89e776ec78d6
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 16:15:04 -0400

    Update FarmAnimals.java

--- ChildFieldTrip/src/com/example/childfieldtrip/FarmAnimals.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/FarmAnimals.java
@@ -44,11 +44,6 @@ public class FarmAnimals extends Activity{
 			}
 	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.farm_animals, menu);
-		return true;
-	}
+
 
 }
