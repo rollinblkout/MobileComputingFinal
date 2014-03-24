17489cdbfe83df0b22b9b851ba9a7d17a12c1c4d
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 16:16:22 -0400

    Update Reptiles.java

--- ChildFieldTrip/src/com/example/childfieldtrip/Reptiles.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Reptiles.java
@@ -35,11 +35,6 @@ public class Reptiles extends Activity {
 		});
 			}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.reptiles, menu);
-		return true;
-	}
+
 
 }
