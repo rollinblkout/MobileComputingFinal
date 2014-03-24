312f130c3111e0226b8173f00f97b2594f6bf9e3
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 12:30:24 -0400

    Create Birds.java

---
+++ ChildFieldTrip/src/com/example/childfieldtrip/Birds.java
@@ -0,0 +1,44 @@
+package com.example.childfieldtrip;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class Birds extends Activity {
+	ListView lvBirds;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_birds);
+		
+		lvBirds= (ListView) findViewById(R.id.lvB);
+
+		lvBirds.setOnItemClickListener(new OnItemClickListener() {
+		
+			@Override
+			public void onItemClick(AdapterView<?> adapter, View v,
+					int position, long id) {
+				String[] items = { "Parrot"};
+				if (items[position].equals("Parrot")){
+					
+						Intent i = new Intent(getApplicationContext(),Parrot.class);
+						startActivity(i);
+				}
+					
+			}
+		});
+			}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.birds, menu);
+		return true;
+	}
+
+}
