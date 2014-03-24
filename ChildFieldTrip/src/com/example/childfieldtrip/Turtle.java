a0dbbe910c725c615b8b6e26882db34fac12ba54
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 12:32:18 -0400

    Create Turtle.java

---
+++ ChildFieldTrip/src/com/example/childfieldtrip/Turtle.java
@@ -0,0 +1,27 @@
+package com.example.childfieldtrip;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class Turtle extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_turtle);
+	}
+	public void goToCamera(View arg0) {
+		Intent i = new Intent(getApplicationContext(),CameraActivity.class);
+		startActivity(i);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.turtle, menu);
+		return true;
+	}
+
+}
