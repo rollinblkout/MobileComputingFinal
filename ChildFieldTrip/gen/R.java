d08f1382b9676f96b3a548135a40e10836c894b0
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 14:08:38 -0400

    Update R.java

--- ChildFieldTrip/gen/R.java
+++ ChildFieldTrip/gen/R.java
@@ -1,4 +1,3 @@
-
 public final class R {
     public static final class array {
         public static final int bird=0x7f060003;
@@ -19,16 +18,9 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int camera=0x7f020000;
-        public static final int cow=0x7f020001;
-        public static final int dog=0x7f020002;
-        public static final int horse=0x7f020003;
+
         public static final int ic_launcher=0x7f020004;
-        public static final int kitten=0x7f020005;
-        public static final int parrot=0x7f020006;
-        public static final int puppy=0x7f020007;
-        public static final int rabbit=0x7f020008;
-        public static final int turtle=0x7f020009;
+  
     }
     public static final class id {
         public static final int action_settings=0x7f09000e;
