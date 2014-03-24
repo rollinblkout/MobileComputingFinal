1377f9347142b76a6afd135563d452c4edcb3fb1
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 16:17:11 -0400

    Update Turtle.java

--- ChildFieldTrip/src/com/example/childfieldtrip/Turtle.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Turtle.java
@@ -17,11 +17,6 @@ public class Turtle extends Activity {
 		Intent i = new Intent(getApplicationContext(),CameraActivity.class);
 		startActivity(i);
 	}
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.turtle, menu);
-		return true;
-	}
+
 
 }
