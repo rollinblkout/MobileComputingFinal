7f825e553742d13be416a7f18153a68b1eee2545
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 12:12:45 -0400

    Create Dog.java

---
+++ ChildFieldTrip/src/com/example/childfieldtrip/Dog.java
@@ -0,0 +1,28 @@
+package com.example.childfieldtrip;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class Dog extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_dog);
+	}
+
+	public void goToCamera(View arg0) {
+		Intent i = new Intent(getApplicationContext(),CameraActivity.class);
+		startActivity(i);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.dog, menu);
+		return true;
+	}
+
+}
