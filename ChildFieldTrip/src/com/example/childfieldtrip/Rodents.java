188d56764bcffbdba30ee15be31b2b42b5edc99f
redgateg1 <redgateg1@users.noreply.github.com>
2014-03-24 16:16:52 -0400

    Update Rodents.java

--- ChildFieldTrip/src/com/example/childfieldtrip/Rodents.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Rodents.java
@@ -38,11 +38,6 @@ public class Rodents extends Activity {
 		);
 			}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.rodents, menu);
-		return true;
-	}
+
 
 }
