4d371c9278edfb8ad0bacf4a14b698c078569070
Gabrielle Redgate <gabrielleredgate@192.168.1.112>
2014-03-25 01:12:07 -0400

    Added Animal Title

--- ChildFieldTrip/src/com/example/childfieldtrip/MainActivity.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.childfieldtrip;
 
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -8,21 +10,20 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  setContentView(R.layout.activity_main);
+ }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-	/*public void goToAnimalScreen(View arg0) {
-		Intent i = new Intent(getApplicationContext(),AnimalTitle.class);
-		startActivity(i);
-	}
-*/
+ @Override
+ public boolean onCreateOptionsMenu(Menu menu) {
+  // Inflate the menu; this adds items to the action bar if it is present.
+  getMenuInflater().inflate(R.menu.main, menu);
+  return true;
+ }
+ public void goToAnimalScreen(View arg0) {
+  Intent i = new Intent(getApplicationContext(),AnimalTitle.class);
+  startActivity(i);
+ }
 }
