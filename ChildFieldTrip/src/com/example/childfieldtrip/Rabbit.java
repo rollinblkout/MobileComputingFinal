6ccc4323f96ce7abba183ac2ca1408f27db4d621
Gabrielle Redgate <gabrielleredgate@192.168.1.112>
2014-03-25 01:28:59 -0400

    Add Rabbit

---
+++ ChildFieldTrip/src/com/example/childfieldtrip/Rabbit.java
@@ -0,0 +1,29 @@
+package com.example.childfieldtrip;
+
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class Rabbit extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_rabbit);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.rabbit, menu);
+		return true;
+	}
+	public void goToCamera(View arg0) {
+		Intent i = new Intent(getApplicationContext(),CameraActivity.class);
+		startActivity(i);
+	}
+}
