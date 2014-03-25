77d94af428860013a7178064f36839c1018385fe
Gabrielle Redgate <gabrielleredgate@192.168.1.112>
2014-03-25 02:27:38 -0400

    Update to Animal Title Search functino added

--- ChildFieldTrip/src/com/example/childfieldtrip/AnimalTitle.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/AnimalTitle.java
@@ -1,9 +1,13 @@
 package com.example.childfieldtrip;
+
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AnimalTitle extends Activity {
 
@@ -19,6 +23,62 @@ public class AnimalTitle extends Activity {
   getMenuInflater().inflate(R.menu.animal_title, menu);
   return true;
  }
+ 
+ public void search(View arg0){
+	 
+	 EditText message = (EditText)findViewById(R.id.editText1);
+		String animal = message.getText().toString();
+		
+		if (animal.equalsIgnoreCase("dog"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Dog.class);
+			  startActivity(i);
+		}else {
+		
+		if (animal.equalsIgnoreCase("cat"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Cat.class);
+			  startActivity(i);
+		} else {
+		
+		if (animal.equalsIgnoreCase("rabbit"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Rabbit.class);
+			  startActivity(i);
+		} else
+		
+		if (animal.equalsIgnoreCase("horse"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Horse.class);
+			  startActivity(i);
+		} else {
+		
+		if (animal.equalsIgnoreCase("cow"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Cow.class);
+			  startActivity(i);
+		} else {
+		
+		if (animal.equalsIgnoreCase("parrot"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Parrot.class);
+			  startActivity(i);
+		} else {
+		
+		if (animal.equalsIgnoreCase("turtle"))
+		{
+			 Intent i = new Intent(getApplicationContext(),Turtle.class);
+			  startActivity(i);
+		} else {
+			Toast.makeText(getApplicationContext(),"Oops! " + animal + " is not in the database!", Toast.LENGTH_SHORT).show();
+		}
+		}
+		}
+		}
+		}
+		}
+		}
+ 
  public void goToDomesticScreen(View arg0) {
   Intent i = new Intent(getApplicationContext(),Domestic.class);
   startActivity(i);
