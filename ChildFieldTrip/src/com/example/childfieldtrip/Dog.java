4859ed0020d6f81eab212a92338ecce55ed45e0c
Gabrielle Redgate <gabrielleredgate@192.168.1.112>
2014-03-26 00:21:52 -0400

    Added Camera feature though still needs to be able to be stored

--- ChildFieldTrip/src/com/example/childfieldtrip/Dog.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Dog.java
@@ -3,26 +3,44 @@ package com.example.childfieldtrip;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.graphics.Bitmap;
+import android.graphics.Camera;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Dog extends Activity {
-
+	Button Camera;
+	int CAMERA_ACTIVATE = 5; 
+	Camera camera;
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_dog);
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_dog);
 	}
+	        public void goToCamera(View arg0)
+	        {
+	            Intent Intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+	            startActivityForResult(Intent, CAMERA_ACTIVATE);
+	        }
+      
+	
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+{
+    super.onActivityResult(requestCode, resultCode, data);
+    if( requestCode == CAMERA_ACTIVATE)
+    {   
+        Bitmap picture = (Bitmap) data.getExtras().get("data");
+        ImageView image =(ImageView) findViewById(R.id.imageView1);
+        image.setImageBitmap(picture);
+    }
+    else 
+    {
+    	Toast.makeText(getApplicationContext(),"Oops! You forgot to take the picture!", Toast.LENGTH_SHORT).show();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.dog, menu);
-		return true;
-	}
+}
 
-	public void goToCamera(View arg0) {
-				Intent i = new Intent(getApplicationContext(),CameraActivity.class);
-				startActivity(i);
-			}
 }
--- ChildFieldTrip/src/com/example/childfieldtrip/Parakeet.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Parakeet.java
@@ -3,25 +3,41 @@ package com.example.childfieldtrip;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
+import android.graphics.Bitmap;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Parakeet extends Activity {
-
+int CAMERA_ACTIVATE = 5;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_parakeet);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.parakeet, menu);
-		return true;
-	}
-	public void goToCamera(View arg0) {
-		Intent i = new Intent(getApplicationContext(),CameraActivity.class);
-		startActivity(i);
-	}
+    public void goToCamera(View arg0)
+    {
+        Intent Intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(Intent, CAMERA_ACTIVATE);
+    }
+
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+{
+super.onActivityResult(requestCode, resultCode, data);
+if( requestCode == CAMERA_ACTIVATE)
+{   
+Bitmap picture = (Bitmap) data.getExtras().get("data");
+ImageView image =(ImageView) findViewById(R.id.imageView1);
+image.setImageBitmap(picture);
+}
+else 
+{
+Toast.makeText(getApplicationContext(),"Oops! You forgot to take the picture!", Toast.LENGTH_SHORT).show();
+}
+
+}
+
 }
