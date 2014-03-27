7a120e4840d9e1721cf5fb6a42dfd524da08bd1c
Gabrielle Redgate <gabrielleredgate@192.168.1.112>
2014-03-27 14:34:20 -0400

    Test

--- ChildFieldTrip/src/com/example/childfieldtrip/Bear.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Bear.java
@@ -11,7 +11,7 @@ public class Bear extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_bear);
 	}
-
+//good
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
