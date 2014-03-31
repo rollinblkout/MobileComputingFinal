793b02f8c2978cbccba7ca1db0e1b8d68562f9db
Syed Sajjad <syedsjj@gmail.com>
2014-03-31 11:42:22 -0400

    New Animal Search. Added list of the animals in animalsearch.txt. Tried to implement new intent but not successful

--- ChildFieldTrip/src/com/example/childfieldtrip/AnimalTitle.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/AnimalTitle.java
@@ -1,11 +1,19 @@
 package com.example.childfieldtrip;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class AnimalTitle extends Activity {
@@ -23,10 +31,27 @@ public class AnimalTitle extends Activity {
   return true;
  }
  
- public void search(View arg0){
+ public void search(View arg0) throws FileNotFoundException{
 	 
-	 
+	 String animal = ((EditText)findViewById(R.id.textView1)).getText().toString();
+	 Scanner s = new Scanner(new File("animalsearch.txt"));
+	 List<String> list = new ArrayList<String>();
+	 while(s.hasNextLine())
+		{
+			list.add(s.nextLine());
 		}
+		s.close();
+		if (list.contains(animal))
+		{
+			Toast.makeText(getApplicationContext(),"SUCCESS! " + animal + " is in the database!", Toast.LENGTH_SHORT).show();
+			//Intent i = new Inten(getApplicationContext(),animal.class);
+			//startActivity(i);
+		}
+		else
+		{
+			Toast.makeText(getApplicationContext(),"Oops! " + animal + " is not in the database!", Toast.LENGTH_SHORT).show();
+		}
+	}
  
  public void goToDomesticScreen(View arg0) {
   Intent i = new Intent(getApplicationContext(),Domestic.class);
