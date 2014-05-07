3fae9219b60daea018fe3a242568b6dbc263f4a7
Gabrielle Redgate <gabrielleredgate@yahoo.com>
2014-05-07 00:48:51 -0400

    Fixed panther

--- ChildFieldTrip/src/com/example/childfieldtrip/Panther.java
+++ ChildFieldTrip/src/com/example/childfieldtrip/Panther.java
@@ -1,20 +1,176 @@
 package com.example.childfieldtrip;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Panther extends Activity {
-
+	private int currentImage = 0;
+	private int numImages = 3;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_panther);
-	}
+		 File stuff = Environment.getExternalStorageDirectory();
+		    
+		    File thing = new File (stuff.getAbsolutePath());
+		    File file = new File (thing, "pantherimage.jpg");
+		    String path = stuff + "/pantherimage.jpg";
+		    if (file.exists())
+		    {
+		    		Date lastModDate = new Date(file.lastModified());  
+		    		String date = lastModDate.toString();
+		            TextView t = (TextView)findViewById(R.id.textView3); 
+		            BitmapFactory.Options options = new BitmapFactory.Options();
+		            options.inSampleSize = 8;
+		            t.setText("Last picture taken on: " + date);
+		    		Bitmap initial = BitmapFactory.decodeFile(path, options);
+		    		 ImageView imageI =(ImageView) findViewById(R.id.imageView1);
+		    	        imageI.setImageBitmap(initial);
+		   
+		    }
+		}
+		        public void goToCamera(View arg0)
+		        {
+		        	File directory = new File(Environment.getExternalStorageDirectory(), "pantherimage.jpg");
+		        	File directory2 = new File(Environment.getExternalStorageDirectory(), "oldpantherimage.jpg");
+		        	if(directory.exists()){
+		        		if (directory2.exists()) {
+		        			directory2.delete();
+		        			File blah = new File(Environment.getExternalStorageDirectory(), "oldpantherimage.jpg");
+		        			directory.renameTo(blah);
+		        			directory.delete();
+		        		} else {
+		        			
+		        		directory.renameTo(directory2);
+		        		directory.delete();
+		        		
+		        	}}else {
+		        			try{
+		        				directory.createNewFile();
+		        			} catch (IOException e){
+		        				e.printStackTrace();
+		        			}
+		        		}
+		        	Uri image = Uri.fromFile(directory);
+		            Intent Intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		            Intent.putExtra(MediaStore.EXTRA_OUTPUT, image);
+		            startActivityForResult(Intent, 5);
+		        }
+	      
+		        public void onPictureClick(View v) {
+		        	
+		        Bitmap initial = null;
+		        Bitmap init = null;
 
+	    	    File stuff2 = Environment.getExternalStorageDirectory();
+	    	    
+	    	    File thing2 = new File (stuff2.getAbsolutePath());
+	    	    File file2 = new File (thing2, "oldpantherimage.jpg");
+	    	    if (file2.exists())
+	    	    {
+	    	    
+	      	          String p2 = thing2 + "/oldpantherimage.jpg";
+	      	        BitmapFactory.Options opt = new BitmapFactory.Options();
+		            opt.inSampleSize = 8;
+	      	      init = BitmapFactory.decodeFile(p2, opt);
+	   
+
+	      	    	   
+	      	
+	    	    }
+		    	    File stuff = Environment.getExternalStorageDirectory();
+		    	    
+		    	    File thing = new File (stuff.getAbsolutePath());
+		    	    File file = new File (thing, "pantherimage.jpg");
+		        	  if (file.exists())
+		      	    {
+		      	    		Date lastModDate = new Date(file.lastModified());  
+		      	    		String date = lastModDate.toString();
+		      	            TextView t = (TextView)findViewById(R.id.textView3);  
+		      	            t.setText("Last picture taken on: " + date);
+		      	          String p = thing + "/pantherimage.jpg";
+		      	        BitmapFactory.Options opt = new BitmapFactory.Options();
+			            opt.inSampleSize = 8;
+		      	      initial = BitmapFactory.decodeFile(p, opt);
+		   
+
+		      	    	   
+		    
+		      	    }
+		 
+		        	  
+		            //Increase Counter to move to next Image
+		            currentImage++;
+		            currentImage = currentImage % numImages;
+		            		 ImageView imageI =(ImageView) findViewById(R.id.imageView1);
+		            //Set the image depending on the counter.
+		            switch (currentImage) {
+		            case 0:  imageI.setImageResource(R.drawable.panther);
+		                     break;
+		            case 1: if (file.exists())
+	            	{ imageI.setImageBitmap(initial); }
+		            else { imageI.setImageResource(R.drawable.panther);
+		            currentImage = 0;
+		            }
+	 	            break;
+		            case 2: if (file2.exists())
+	            	{ imageI.setImageBitmap(init); }
+		            else { imageI.setImageResource(R.drawable.panther);
+		            currentImage = 0;
+		            }
+	 	            break;
+		            default: imageI.setImageResource(R.drawable.panther);
+		            currentImage = 0;
+		            }
+		            }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) 
+	{
+	    super.onActivityResult(requestCode, resultCode, data);
+	    if( requestCode == 5)
+	    {   
+
+	    File stuff = Environment.getExternalStorageDirectory();
+	    
+	    File thing = new File (stuff.getAbsolutePath());
+	    File file = new File (thing, "pantherimage.jpg");
+	    String path = stuff + "/pantherimage.jpg";
+	    if (file.exists())
+	    {
+	    	Date lastModDate = new Date(file.lastModified());  
+	    		String date = lastModDate.toString();
+	            TextView t = (TextView)findViewById(R.id.textView3); 
+	            BitmapFactory.Options options = new BitmapFactory.Options();
+	            options.inSampleSize = 8;
+	            t.setText("Last picture taken on: " + date);
+	    		Bitmap initial = BitmapFactory.decodeFile(path, options);
+	    		 ImageView imageI =(ImageView) findViewById(R.id.imageView1);
+	    	        imageI.setImageBitmap(initial);
+	    
+	    }
+	}
+	else 
+	{
+	Toast.makeText(getApplicationContext(),"Oops! You forgot to take the picture!", Toast.LENGTH_SHORT).show();
+	}
+	}
 
 @Override
 public boolean onCreateOptionsMenu(Menu menu) {
@@ -54,5 +210,4 @@ public boolean onOptionsItemSelected(MenuItem item) {
             return super.onOptionsItemSelected(item);
     }
 }
-
 }
